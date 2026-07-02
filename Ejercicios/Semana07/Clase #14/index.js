84d4fbfc843d3b3f3a12b0593fbeba589c68245a
JoseAndres0508 <ortiz3558jo@gmail.com>
2026-07-02 10:35:12 -0600

    docs: comentar ejemplo de arreglo disperso (sparse array)

--- Ejercicios/Semana07/Clase #14/index.js
+++ Ejercicios/Semana07/Clase #14/index.js
@@ -1,4 +1,13 @@
+/*
 const numeros = [40, 30, 1, 5, 8, 100];
 
 console.log(numeros.sort((a, b) => a - b));
 console.log(numeros.sort((a, b) => b - a));
+*/
+
+/*
+let arr = ["a","b","c"];
+arr[7] = "z";
+arr.length;
+*/
+
